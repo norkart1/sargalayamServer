37cf495edec0f6140e9bb17685f8b9f71f65208f
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-11-08 23:49:09 +0530

    password && email changed

--- Model/teams.js
+++ Model/teams.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true },
+  //image: { type: String, required: true },
   
   createdAt: { type: Date, default: Date.now },
   programs: [
